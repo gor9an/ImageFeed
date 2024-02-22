859e0642e4769eefe183098c10ed7e7650b23f82
gor9an <gor9an@mail.ru>
2024-02-23 01:48:31 +0300

    add ProgressView with KVO and didTapBackButton() logic

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -17,7 +17,9 @@ protocol WebViewViewControllerDelegate: AnyObject {
 
 
 final class WebViewViewController: UIViewController {
-    private var webView: WKWebView?
+    private var webView = WKWebView()
+    private let progressView = UIProgressView()
+
     weak var delegate: WebViewViewControllerDelegate?
 
     override func viewDidLoad() {
@@ -33,25 +35,58 @@ final class WebViewViewController: UIViewController {
         ]
         let url = urlComponents.url!
         let request = URLRequest(url: url)
-        webView?.load(request)
+        webView.load(request)
         
-        webView?.navigationDelegate = self
+        webView.navigationDelegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+    }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        webView.removeObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+//    MARK: - Private Functions
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
     private func configureWebView() {
-        webView = WKWebView()
-        guard let webView else { return }
         webView.backgroundColor = .ypWhite
         
         let backButton = UIButton(type: .custom)
         backButton.setImage(UIImage(named: "login_back_button"), for: .normal)
         backButton.addTarget(self, action: #selector(Self.didTapBackButton), for: .touchUpInside)
         
+        progressView.progressTintColor = .ypBlack
+        
         webView.translatesAutoresizingMaskIntoConstraints = false
         backButton.translatesAutoresizingMaskIntoConstraints = false
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(webView)
         view.addSubview(backButton)
+        view.addSubview(progressView)
         
         NSLayoutConstraint.activate([
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -63,15 +98,20 @@ final class WebViewViewController: UIViewController {
             backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 9),
             backButton.widthAnchor.constraint(equalToConstant: 24),
             backButton.heightAnchor.constraint(equalToConstant: 24),
+            
+            progressView.topAnchor.constraint(equalTo: backButton.bottomAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
     }
     
     @objc
     private func didTapBackButton() {
-        
+        delegate?.webViewViewControllerDidCancel(self)
     }
 }
 
+//MARK: - WKNavigationDelegate
 extension WebViewViewController: WKNavigationDelegate {
     func webView(
         _ webView: WKWebView,
@@ -79,7 +119,7 @@ extension WebViewViewController: WKNavigationDelegate {
         decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
     ) {
         if let code = code(from: navigationAction) {
-            //TODO: process code
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
             decisionHandler(.cancel)
         } else {
             decisionHandler(.allow)
