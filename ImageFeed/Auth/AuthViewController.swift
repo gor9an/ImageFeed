8824846e81ad61d0b5bd8f51a438dfcba395fc96
gor9an <gor9an@mail.ru>
2024-03-08 18:00:02 +0300

    fix the race condition using the Main Thread and UI lock

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -84,11 +84,11 @@ extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         vc.dismiss(animated: true)
         
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
         OAuth2Service.shared.fetchOAuthToken(code: code, completion: { [weak self] result in
             guard let self = self else { return }
             
-            ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
             
             switch result {
             case .success(let access_token):
--- ImageFeed/Auth/OAuth2Service.swift
+++ ImageFeed/Auth/OAuth2Service.swift
@@ -7,12 +7,23 @@
 
 import Foundation
 
+enum AuthServiceError: Error {
+    case invalidRequest
+}
+
 final class OAuth2Service {
     static let shared = OAuth2Service()
     private init() { }
     
-    func makeOAuthTokenRequest(code: String) -> URLRequest {
-        var urlComponents = URLComponents(string: UnsplashUrl)!
+    private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
+    func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: UnsplashUrl) else {
+            assertionFailure("Failed to create URL")
+            return nil
+        }
         
         urlComponents.queryItems = [
             URLQueryItem(name: "client_id", value: AccessKey),
@@ -22,15 +33,34 @@ final class OAuth2Service {
             URLQueryItem(name: "grant_type", value: "authorization_code"),
         ]
         
-        let url = urlComponents.url!
+        guard let url = urlComponents.url else {
+            assertionFailure("Failed to create URL")
+            return nil
+        }
+        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         return request
     }
     
     func fetchOAuthToken(code: String, completion: @escaping (Result<String,Error>) -> Void) {
-        let request = makeOAuthTokenRequest(code: code)
-        let task = URLSession.shared.data(for: request, completion: { result in
+        assert(Thread.isMainThread)
+        
+        guard lastCode != code else {
+            completion(.failure(AuthServiceError.invalidRequest))
+            return
+        }
+        
+        task?.cancel()
+        
+        lastCode = code
+        
+        guard let request = makeOAuthTokenRequest(code: code) else {
+            completion(.failure(AuthServiceError.invalidRequest))
+            return
+        }
+        
+        let task = urlSession.data(for: request, completion: { [weak self] result in
             switch result {
             case .success(let data):
                 do { 
@@ -49,9 +79,9 @@ final class OAuth2Service {
                 
                 completion(.failure(error))
             }
+            self?.task = nil
+            self?.lastCode = nil
         })
-        
         task.resume()
-        
     }
 }
